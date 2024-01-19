997ba96a9f0af8580cd653a94870252faf650a4f
PongManao <ath@manaosoftware.com>
2024-01-19 22:40:07 +0700

    Click to add aircraft.

--- src/components/CesiumMap.js
+++ src/components/CesiumMap.js
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import { Viewer, Entity, PointGraphics, Color } from 'resium';
+import React, { useRef, useEffect } from 'react';
+import { Viewer, Entity, PointGraphics, Color,PolylineCollection,Camera } from 'resium';
 import * as Cesium from 'cesium';
 
 const CesiumMap = () => {
@@ -14,22 +14,39 @@ const CesiumMap = () => {
     // Extract latitude and longitude
     const centerLatitude = Cesium.Math.toDegrees(cameraPosition.latitude);
     const centerLongitude = Cesium.Math.toDegrees(cameraPosition.longitude);
+    console.log(viewer.entities, centerLatitude, centerLongitude);
+    //const checkExistPosition = viewer.entities.find((element) => element.position.x == centerLatitude && element.position.y == centerLongitude);
+    
     viewer.entities.add({
       position: Cesium.Cartesian3.fromDegrees(centerLongitude, centerLatitude),
-      point: {
-        pixelSize: 10,
-        color: Cesium.Color.RED,
-      },
+    //   point: {
+    //     pixelSize: 10,
+    //     color: Cesium.Color.RED,
+    //   },
+      model: {
+        uri: 'images/Cesium_Air.glb', // Provide the path to your airplane GLTF model
+        scale: 2.0, // Adjust scale as needed,
+        minimumPixelSize : 64
+    },
     });
   };
 
+  const center = Cesium.Cartesian3.fromDegrees(14.492666542936876, 100.5056808521216);
+  const centerPoint = { lat: 13.7563, lon: 100.5018 }; // Coordinates for Thailand (Bangkok)
   return (
     <div>
         <button onClick={addEntityAtCenter}>Add Entity at Center</button>
           <div >
               <Viewer ref={viewerRef} >
-                  {/* Other map configurations and components go here */}
-                  <Entity />
+              {/* <Camera
+                defaultPosition={centerPoint}
+                defaultCenter={centerPoint}
+                defaultZoom={7}
+            />
+            <Entity
+                position={centerPoint}
+                point={{ pixelSize: 10, color: Cesium.Color.RED }}
+            /> */}
               </Viewer>
           </div>
     </div>
